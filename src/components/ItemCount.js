726de31571c18f39e4f70b5ad471676d815b868a
ayelen aguilar <aye47aguilar@gmail.com>
2022-09-13 00:47:11 -0300

    contador

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,27 +1,42 @@
+import "../App.css";
 import { useState } from "react";
 const ItemCount=({titulo})=>{
 
     const[cantidad, setCant]= useState(0);
 
     const suma=()=>{
-        if(cantidad<5)
-        setCant(cantidad+1);
+        if(cantidad<5){
+        setCant(cantidad+1)}
+    }
     
+    const resta=()=>{
+        if (cantidad >=0){
     
+    setCant(cantidad-1)}
 
+    }
+    const agregar = () => {
+        setCant (cantidad);
+    
+            if(cantidad !== 0){
+                alert(`Se han agregado al carrito ${cantidad} productos`)
+                setCant(1);
+            }
+    
+    }
 
-}
     return(
         <div>
-        <h3>{titulo}</h3>
+        <h3 className="titulo">{titulo}</h3>
         
-        <button onClick={suma}>+</button>{cantidad}
-        <button>-</button>
-        <button></button>
+        <button className="boton" onClick={suma}>+</button>
+        <span>{cantidad}</span>
+        <button className="boton" onClick={resta}>-</button>
+        <button className="boton btn" onClick={agregar}>Agregar al carrito</button>
         </div>
 
     );
-}
 
+    }
 
 export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,20 +1,12 @@
 import ItemCount from "./ItemCount";
 
 const ItemListContainer=(props)=>{
-    const cantidad= (id)=>{
-        alert(`Agregó ${id} productos al carrito`);
-
-    }
-
+    
     return(
     <div>
     <div>{props.greeting}</div>
     <ItemCount
     titulo="Taza de gatito"
-    
-    cantidad={cantidad}
-    id={1}
-    
     />
     </div>
     );
